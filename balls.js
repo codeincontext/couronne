782432a2ecfce06211040a3c6663606b70e3dab6
skattyadz <adz@skatty.me>
2011-02-05 01:23:26 +0000

    Detect clicking on the cue ball

--- balls.js
+++ balls.js
@@ -1,27 +1,66 @@
 var context;
-var x=100;
-var y=200;
-var dx=5;
-var dy=5;
+var cx=100;
+var cy=200;
+var vx=0;
+var vy=0;
+var mx;
+var my;
+var offset = $('canvas').offset();
+var shooting = false;
 
-function init()
-{
+function init(){
   context= myCanvas.getContext('2d');
-  setInterval(draw,10);
+  setInterval(tick,10);
 }
+function tick(){
+  draw();
+  if( cx<50 || cx>250) vx=-vx;
+  if( cy<50 || cy>250) vy=-vy;
+  cx+=vx;
+  cy+=vy;
+}
+function draw(){
+  context.clearRect(0,0, 500,500);
 
-function draw()
-{
-  context.clearRect(0,0, 300,300);
+  if (shooting) {
+    context.strokeStyle = '#f00';
+    context.lineWidth   = 10;
+
+    context.beginPath();
+    context.moveTo(cx, cy);
+    context.lineTo(mx, my);
+
+    context.stroke();
+    context.closePath();
+  }
+
   context.beginPath();
   context.fillStyle="#0000ff";
-  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-  context.arc(x,y,20,0,Math.PI*2,true);
+  context.arc(cx,cy,20,0,Math.PI*2,true);
   context.closePath();
   context.fill();
-  if( x<0 || x>300) dx=-dx;
-  if( y<0 || y>300) dy=-dy;
-  x+=dx;
-  y+=dy;
 }
+function dist(x, y, X, Y){
+  x_diff = Math.pow( (x-X), 2 );
+  y_diff = Math.pow( (y-Y), 2 );
+  dist1 = Math.sqrt( x_diff + x_diff );
+  if (dist1<0) {dist1 = -dist1;}
+  return dist1;
+}
+$('canvas').mousedown(function(e){
+  mx = e.pageX - offset.left
+  my = e.pageY - offset.top
+  if (dist(mx,my, cx, cy) < 20){
+    shooting = true;
+  }
+});
+$('canvas').mouseup(function(e){
+  shooting = false;
+});
+$('canvas').mousemove(function(e){
+  mx = e.pageX - offset.left
+  my = e.pageY - offset.top
+});
+
+
 window.onload += init();
